d6095abcc0a6984e97223b2de4faf45e373cdace
Jorg van Maaren <j.vanmaaren@labdigital.nl>
2022-11-03 10:16:16 +0100

    Create example project

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      Updating this text should update the DOM (and also not reload the entire
+      page).
+    </div>
+  );
+};
+
+export default App;
