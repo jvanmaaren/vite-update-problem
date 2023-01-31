ea1888502b58df61513d0bff8d5eb38ccbc57704
Jorg van Maaren <j.vanmaaren@labdigital.nl>
2023-01-31 16:45:08 +0100

    Use latest beta versions
    Of vite and vitejs/plugin-react

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import "vite/modulepreload-polyfill";
 import App from "./App";
 import { createRoot } from "react-dom/client";
+import React from "react";
 
 const container = document.getElementById("app");
 
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
